49044f100da83c44d5d56e290b65c645ac6f5d72
Șerban Cârjan <serban.carjan@stanga.net>
2016-08-25 18:23:12 +0300

    work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,22 @@
 import animateBackground from './connectThree.js';
+
+const title = document.querySelector('.Title');
+const tabs = document.querySelectorAll('.js-navTab');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    if (!title.classList.contains('Title--collapsed')) {
+      title.classList.add('Title--collapsed');
+    }
+
+    // e.stopPropagation();
+    // console.log(e);
+  });
+});
+
+// window.tabClicked = function tabClicked(ev, el) {
+//   // ev.preventDefault();
+//   console.log('Clicked', ev, el);
+// };
+
 (animateBackground)();
