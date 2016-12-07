89e76652facb717f23ba302ed2f67016b9158dbb
Serban Carjan <serban.carjan@endava.com>
2016-12-07 14:47:23 +0200

    finishes navigation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,28 @@ import Trianglify from 'trianglify';
 import animateBackground from './connectThree.js';
 import TiltFx from './tiltHover.js';
 
+const scrollConfig = {
+  // callback: (destination, target) => {
+  //   destination.querySelectorAll('.NavLink').forEach((navLink) => navLink.classList.remove('NavLink--hidden'));
+  // }
+};
+
+function checkNavLinks() {
+  document.querySelectorAll('.Panel').forEach((panel) => {
+    const panelHeight = panel.offsetHeight;
+    const offsetOfTopFromViewport = panel.getBoundingClientRect().top;
+    const offsetOfBottomFromViewport = panel.getBoundingClientRect().bottom;
+
+    panel.classList[1] === 'Info' && console.log(offsetOfTopFromViewport, offsetOfBottomFromViewport);
+    if (offsetOfTopFromViewport < panelHeight / 2 && offsetOfBottomFromViewport > panelHeight / 2) {
+      panel.querySelectorAll('.NavLink').forEach((navLink) => navLink.classList.contains('NavLink--hidden') && navLink.classList.remove('NavLink--hidden'));
+    } else {
+      panel.querySelectorAll('.NavLink').forEach((navLink) => !navLink.classList.contains('NavLink--hidden') && navLink.classList.add('NavLink--hidden'));
+    }
+  })
+}
+
+
 const pattern = Trianglify({
   // variance: 0.75,
   // seed: 'b4a16',
@@ -22,10 +44,11 @@ const options = {
   }
 };
 
-(function(){
+(function () {
   animateBackground();
-  smoothScroll.init();
+  smoothScroll.init(scrollConfig);
 
-  document.body.appendChild(pattern.svg()).classList.add('Background');
+  // document.body.appendChild(pattern.svg()).classList.add('Background', 'Background--static');
   new TiltFx(tiltHoverableElements, options);
+  window.addEventListener('scroll', checkNavLinks);
 })();
